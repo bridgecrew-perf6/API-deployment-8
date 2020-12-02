deca426ff6b3e876eb5daa8421b6561009af957f
silventesa <sarasilventeifont@gmail.com>
2020-12-02 23:24:09 +0100

    Add model, predict and preprocessing folders

---
+++ app/predict/prediction.py
@@ -0,0 +1,1 @@
+# Create predict() function that will take your preprocessed data as an input and return a price as output.
---
+++ app/preprocessing/cleaning_data.py
@@ -0,0 +1,3 @@
+# Create preprocess() function that will take a new house's data as input and return those data preprocessed as output.
+# Preprocessing: fill the NaNs, handle text data...
+# If data doesn't contain the required information, an error should be returned to the user
